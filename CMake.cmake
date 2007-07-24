04e2b7761e59bafbd6ad4ec779fad0d8946edc77
David Cole <david.cole@kitware.com>
2007-07-24 20:28:13 +0000

    ENH: Add ability to switch on CTEST_TEST_CTEST via script arguments.

--- CMake.cmake
+++ CMake.cmake
@@ -1,2 +1,8 @@
 SET(dir "${CTEST_SCRIPT_DIRECTORY}/../EasyDashboardScripts")
+INCLUDE("${dir}/EasyDashboardVariables.cmake")
+
+IF("${ED_args}" MATCHES "CTEST_TEST_CTEST_ON")
+  ED_APPEND(ED_cache "CTEST_TEST_CTEST:BOOL=ON")
+ENDIF("${ED_args}" MATCHES "CTEST_TEST_CTEST_ON")
+
 INCLUDE("${dir}/EasyDashboard.cmake")
