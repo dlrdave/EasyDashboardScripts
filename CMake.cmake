a77d20281e5f7176d2f73e297a5f402f2d9652c8
David Cole <david.cole@kitware.com>
2008-03-19 23:28:24 +0000

    ENH: Switch default senses of some of the CMake EasyDashboard options...

--- CMake.cmake
+++ CMake.cmake
@@ -1,12 +1,21 @@
 SET(dir "${CTEST_SCRIPT_DIRECTORY}/../EasyDashboardScripts")
 INCLUDE("${dir}/EasyDashboardVariables.cmake")
 
-IF("${ED_args}" MATCHES "BUILD_QtDialog_ON")
-  ED_APPEND(ED_cache "BUILD_QtDialog:BOOL=ON")
-ENDIF("${ED_args}" MATCHES "BUILD_QtDialog_ON")
+# ED_cmd_qmake should be set in Support/EasyDashboardDefaults.cmake on
+# machines that have Qt installed.
+#
+IF(NOT "${ED_cmd_qmake}" STREQUAL "")
+  IF(NOT "${ED_args}" MATCHES "BUILD_QtDialog_OFF")
+    ED_APPEND(ED_cache "BUILD_QtDialog:BOOL=ON")
+  ENDIF(NOT "${ED_args}" MATCHES "BUILD_QtDialog_OFF")
+ENDIF(NOT "${ED_cmd_qmake}" STREQUAL "")
 
-IF("${ED_args}" MATCHES "CTEST_TEST_CTEST_ON")
+IF(NOT "${ED_args}" MATCHES "CTEST_TEST_CTEST_OFF")
   ED_APPEND(ED_cache "CTEST_TEST_CTEST:BOOL=ON")
-ENDIF("${ED_args}" MATCHES "CTEST_TEST_CTEST_ON")
+ENDIF(NOT "${ED_args}" MATCHES "CTEST_TEST_CTEST_OFF")
+
+IF(NOT "${ED_args}" MATCHES "NoPackage")
+  SET(ED_buildtarget "package")
+ENDIF(NOT "${ED_args}" MATCHES "NoPackage")
 
 INCLUDE("${dir}/EasyDashboard.cmake")
