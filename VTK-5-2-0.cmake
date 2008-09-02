533fe5493fdd4ca2281d1a4d20a338e7512ef8e0
David Cole <david.cole@kitware.com>
2008-09-02 14:36:29 +0000

    BUG: Do not allow an update step when building against a non-branch cvs tag. Assume developer has snapped the source tree to the named tag already.

--- VTK-5-2-0.cmake
+++ VTK-5-2-0.cmake
@@ -1,4 +1,10 @@
 SET(ED_source "VTK")
 SET(ED_tag "VTK-5-2-0")
 
+SET(ED_update 0)
+  # When snapping to a tag, the source tree must be updated to
+  # that tag manually prior to running a Nightly dashboard on it.
+  # Otherwise, cvs update with both a non-branch tag *and* a
+  # -D Nightly start time timestamp yields a confused cvs update...
+
 INCLUDE("${CTEST_SCRIPT_DIRECTORY}/VTK.cmake")
