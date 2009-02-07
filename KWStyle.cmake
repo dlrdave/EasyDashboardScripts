5284eb864da9d595143e87336ddaf903da3029ed
David Cole <david.cole@kitware.com>
2009-02-07 18:18:15 +0000

    ENH: Update KWStyle script since it now has a CTestConfig and point to its source repository.

--- KWStyle.cmake
+++ KWStyle.cmake
@@ -1,11 +1,4 @@
-# KWStyle "CTestConfig.cmake equivalents" since that file does not yet exist in KWStyle:
-#
-SET(CTEST_PROJECT_NAME "KWStyle")
-SET(CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
-SET(CTEST_DROP_METHOD "http")
-SET(CTEST_DROP_SITE "public.kitware.com")
-SET(CTEST_DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
-SET(CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/cgi-bin/SubmitKWStyle.cgi")
+SET(ED_source_repository ":pserver:anoncvs:@public.kitware.com:/cvsroot/KWStyle")
 
 SET(dir "${CTEST_SCRIPT_DIRECTORY}/../EasyDashboardScripts")
 INCLUDE("${dir}/EasyDashboard.cmake")
